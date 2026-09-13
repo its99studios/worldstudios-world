0e9bb72097028fc0230fe1c22996779a60cffc09
Harley Sawyer <worldstudiosanimations@gmail.com>
2026-09-13 14:09:59 -0700

    Fix Voided End Stone hitbox

--- src/main/java/net/mcreator/worldstudiosworld/block/VoidedEndStoneBlock.java
+++ src/main/java/net/mcreator/worldstudiosworld/block/VoidedEndStoneBlock.java
@@ -1,12 +1,30 @@
 package net.mcreator.worldstudiosworld.block;
 
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.core.BlockPos;
 
 public class VoidedEndStoneBlock extends Block {
+	private static final VoxelShape SHAPE = box(0, 0, 0, 16, 3, 16);
+
 	public VoidedEndStoneBlock(BlockBehaviour.Properties properties) {
-		super(properties.mapColor(MapColor.COLOR_GRAY).strength(3f, 9f).requiresCorrectToolForDrops().instrument(NoteBlockInstrument.BASEDRUM));
+		super(properties.mapColor(MapColor.COLOR_GRAY).strength(3f, 9f).requiresCorrectToolForDrops().noOcclusion().isRedstoneConductor((bs, br, bp) -> false).instrument(NoteBlockInstrument.BASEDRUM));
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		return SHAPE;
+	}
+
+	@Override
+	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		return Shapes.empty();
 	}
 }
